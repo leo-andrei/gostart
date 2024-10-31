6363b725d8f5837d51b488b07c693fc9973a6655
Andrei Leonte <aleonte@fluidtrends.com>
2024-10-31 15:18:49 +0200

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/leo-andrei/gostart
+
+go 1.22.5
---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "log"
+
+func main() {
+	// This is a placeholder for the main function
+	log.Printf("Hello, World!")
+}
